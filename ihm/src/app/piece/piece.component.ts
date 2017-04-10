f532f132c3d89da663092aef22255bc588ed1726
CBaty <camille.defroberville@gmail.com>
2017-04-10 15:21:25 +0200

    piece and player component very beginning

--- ihm/src/app/piece/piece.component.ts
+++ ihm/src/app/piece/piece.component.ts
@@ -1,3 +1,24 @@
 /**
- * Created by AELION on 10/04/2017.
+ * Created by Camille on 10/04/2017.
  */
+import { Component, Input, Output,EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-piece',
+  templateUrl: '',
+  styleUrls: ['']
+})
+
+export class PieceComponent{
+@Input() position: number;
+@Input() set stateOnBoard(value: boolean){
+  while(this.position !== this.positionInitial+64){
+    this.state = true;
+  }
+  this.state = false;
+};
+
+state: boolean;
+positionInitial: number;
+}
--- ihm/src/app/player/player.component.ts
+++ ihm/src/app/player/player.component.ts
@@ -1,3 +1,21 @@
 /**
- * Created by AELION on 10/04/2017.
+ * Created by Camille on 10/04/2017.
  */
+import { Component, Input, Output,EventEmitter,forEach } from '@angular/core';
+import {pieceComponent} from '../piece/piece.component';
+
+
+@Component({
+  selector: 'app-player',
+  templateUrl: '',
+  styleUrls: ['']
+})
+export class PlayerComponent{
+  @Input() pieces: pieceComponent[];
+  isWinner: boolean;
+  @Output()
+
+
+  }
+
+}
