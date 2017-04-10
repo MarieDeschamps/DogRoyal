3593698c44a1d3468ccd85102ca9601a5ad5023e
saramencor <sara.mendez.cortinas@gmail.com>
2017-04-10 12:26:16 +0200

    files ts

--- ihm/src/app/card/card.component.ts
+++ ihm/src/app/card/card.component.ts
@@ -1,3 +1,14 @@
 /**
  * Created by AELION on 10/04/2017.
  */
+import { Component , Input} from '@angular/core';
+
+@Component({
+  selector:'app-card',
+  template : '',
+  style: ['']
+})
+
+export class CardComponent{
+
+}
