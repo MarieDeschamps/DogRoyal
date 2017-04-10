593016b27a693ded99d4ae24836023961bac7f1d
CBaty <camille.defroberville@gmail.com>
2017-04-10 16:12:17 +0200

    working on pion

--- ihm/src/app/piece/piece.component.ts
+++ ihm/src/app/piece/piece.component.ts
@@ -12,13 +12,13 @@ import { Component, Input, Output} from '@angular/core';
 
 export class PieceComponent{
 @Input() position: number;
-@Input() set stateOnBoard(value: boolean){
-  while(this.position !== this.positionInitial+64){
+state: boolean =false;
+@Output() set stateOnBoard(value: boolean){
+  
+  while(this.position >= this.positionInitial && this.position !== this.positionInitial+64){
     this.state = true;
   }
   this.state = false;
 };
-
-state: boolean;
 positionInitial: number;
 }
