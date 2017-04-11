5a551b93a2212d5209bbd0fcbc4825bf7400cc00
jbog77 <bogota.jeremy@gmail.com>
2017-04-11 14:53:20 +0200

    Update Services Classes

---
+++ back_end/src/main/java/io/dog/service/PlayerService.java
@@ -0,0 +1,67 @@
+package io.dog.service;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import io.dog.dao.CardDao;
+import io.dog.dao.PieceDao;
+import io.dog.dto.Card;
+import io.dog.dto.Piece;
+import io.dog.dto.Player;
+
+@Stateless
+@Named
+public class PlayerService {
+	CardDao cdao;
+	PieceDao pdao;
+
+	@PersistenceContext
+	EntityManager em;
+
+	@PostConstruct
+	void after() {
+		System.out.println("=============== @POSTCONSTRUCT PLAYER SERVICE ===========");
+		this.cdao = new CardDao(em);
+		this.pdao = new PieceDao(em);
+	}
+
+	public void updatePickedCards(List<Player> players) {
+		// Guards
+		if (players.isEmpty()) {
+			System.out.println("No players have moved");
+			return;
+		}
+
+		for (Player player : players) {
+			for (Card card : player.getCards()) {
+				cdao.updatePickedCards(card.getId(), player.getNumber());
+			}
+		}
+	}
+
+	public void updateDisguardCard(Card card) {
+		cdao.updateDisguardCard(card.getId());
+	}
+
+	public void updatePieces(List<Piece> pieces) {
+		// Guards
+		if (pieces.isEmpty()) {
+			System.out.println("No pieces have moved");
+			return;
+		}
+		
+		for (Piece piece : pieces) {
+			if (piece.isStatus()) {
+				pdao.updateStatus(piece.getId());
+			} else {
+				pdao.updatePosition(piece.getId(), piece.getPosition());
+			}
+		}
+	}
+
+}
