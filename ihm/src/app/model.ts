f8ee941e105d7070533205e1af4eb953f73ee0d0
saramencor <sara.mendez.cortinas@gmail.com>
2017-04-11 16:35:31 +0200

    modifs interface

---
+++ ihm/src/app/model.ts
@@ -0,0 +1,27 @@
+export interface Player {
+  pieces: Piece[];
+  hand: Hand;
+  color: string;
+
+}
+
+export interface Piece{
+  position: number;
+  state : boolean;
+  id:number;
+}
+
+
+export interface Hand extends Array<Card>{
+
+}
+
+export interface Card{
+  value: number;
+  id:number;
+}
+
+export interface Deck{
+  pick : number;
+  discard: number;
+}
