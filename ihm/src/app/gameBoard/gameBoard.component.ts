8e89b3f967710f3aae9907eb62c4a1884b9dcdb8
saramencor <sara.mendez.cortinas@gmail.com>
2017-04-11 11:48:47 +0200

    avances gameboard

--- ihm/src/app/gameBoard/gameBoard.component.ts
+++ ihm/src/app/gameBoard/gameBoard.component.ts
@@ -8,7 +8,8 @@ import {Component, Input, Output} from '@angular/core';
   selector: 'app-gameBoard',
   template: `
     <div class="cases">
-      <div *ngFor='let i of cases; ' class='case' style='border:1px solid black;' [style-background]='colorCase(i)'>
+      <div *ngFor='let i of cases;' class='case' style='border:1px solid black;' [style.backgroundColor]='colorCase(i)'>
+        {{i}}
       </div>
     </div>
   `,
@@ -25,8 +26,7 @@ import {Component, Input, Output} from '@angular/core';
     }
 
     .piece {
-      width: 3em;
-      height: 3em;
+
       display: block;
       margin: 0 auto;
     }
@@ -34,7 +34,6 @@ import {Component, Input, Output} from '@angular/core';
 })
 // <div *ngIf='position==i' class="piece" [style.backgroundColor]=' colorCase(i)'></div>
 export class GameBoardComponent {
-
   @Input() position: number [][];
 
   @Input() set nbCases(value: number) {
@@ -45,23 +44,30 @@ export class GameBoardComponent {
     ;
   };
 
-  colorCase(i) {
-    let x = this.position.length;
-    let t = this.cases.length;
-    for (let j = 0; j < x; j++) {
-      let nbCases = this.cases.length;
+  cases = [];
 
-      for (let k = 0; k < t; t++) {
-        let red = Math.floor(((nbCases - 1 - k) * 255) / (nbCases - 1));
-        let green = Math.floor((k * 255) / (nbCases - 1))
-        if (this.position[j][k] = this.position[j][i]) {
-          return `rgb(${red},${green},0)`;
+  colorCase(i) {
+    if (!this.position || !this.cases)
+      return 'pink';
+
+    let nbJoueurs = this.position.length;
+    let nbCases = this.cases.length;
+
+    console.log(nbCases);
+    console.log(nbJoueurs);
+
+    for (let j = 0; j < nbJoueurs; j++) {
+      for (let p = 0; p < this.position[j].length; p++) {
+        if (this.position[j][p] == i) {
+          let red = (j * 3 * 17 * 23 + 50) % 256;
+          let green = (nbJoueurs * j * j * 31 + 50) % 256;
+          return `rgb(${red},${green},20)`;
         }
       }
     }
-  }
 
-  cases = [];
+    return 'white';
+  }
 }
 
 
