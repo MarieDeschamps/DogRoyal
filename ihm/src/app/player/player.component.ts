b7eb3976892242bf06b1f18de50a7e987f545175
CBaty <camille.defroberville@gmail.com>
2017-04-11 08:49:11 +0200

    to make it compile

--- ihm/src/app/player/player.component.ts
+++ ihm/src/app/player/player.component.ts
@@ -2,7 +2,7 @@
  * Created by Camille on 10/04/2017.
  */
 import { Component, Input, Output} from '@angular/core';
-import {PieceComponent} from '../piece/piece.component';
+//import {PieceComponent} from '../piece/piece.component';
 
 
 @Component({
@@ -11,7 +11,7 @@ import {PieceComponent} from '../piece/piece.component';
   styleUrls: ['']
 })
 export class PlayerComponent{
-  @Input() pieces: PieceComponent[];
+  @Input() pieces: number[];
   isWinner: boolean;
  // @Output()
 
