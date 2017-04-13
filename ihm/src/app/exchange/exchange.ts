5744b58d0627f5a71c5db7631d7238b6cce39ac7
saramencor <sara.mendez.cortinas@gmail.com>
2017-04-13 11:36:47 +0200

    connexion exchange

--- ihm/src/app/exchange/exchange.ts
+++ ihm/src/app/exchange/exchange.ts
@@ -1,10 +1,11 @@
 import {Http, Response} from "@angular/http";
 import "rxjs/add/operator/toPromise";
+import {Injectable} from "@angular/core";
 
 
-
+@Injectable()
 export class Exchange {
-  beginPath = "dog/";
+  beginPath = "http://10.31.0.17:8080/back_end_-0.0.1-SNAPSHOT/game/dog/";
 
   constructor(private http: Http) {
   }
@@ -17,46 +18,46 @@ export class Exchange {
 
 
   create(nbPlayers, nbPiecesByPlayer) {
-    return this.http.post(this.beginPath + "create/{{nbPlayers}}/{{nbPiecesByPlayer}}","")
-        .toPromise()
-        .then((response: Response) => response.json());
+    return this.http.post(this.beginPath + "create/" + nbPlayers + "/" + nbPiecesByPlayer, JSON.stringify({}))
+      .toPromise()
+      .then((response: Response) => response.json());
   }
 
   /*load (game) {
    return this.http.post(this.beginPath + "load/{{game}}","")
-        .toPromise()
-        .then((response: Response) => response.json());
-    }*/
+   .toPromise()
+   .then((response: Response) => response.json());
+   }*/
 
   load() {
-    return this.http.post(this.beginPath + "load","")
-        .toPromise()
-        .then((response: Response) => response.json());
+    return this.http.post(this.beginPath + "load", "")
+      .toPromise()
+      .then((response: Response) => response.json());
   }
 
   pick5() {
     return this.http.put(this.beginPath + "pick5", "")
-        .toPromise()
-        .then((response: Response) => response.json());
+      .toPromise()
+      .then((response: Response) => response.json());
   }
 
   play(player, card, piece) {
     let data = {
-        "player": {
-            "id":player.id
-        },
-        "card" : {
-            "id":card.id
-        },
-        "piece": {
-            "id":piece.id 
-        }
+      "player": {
+        "id": player.id
+      },
+      "card": {
+        "id": card.id
+      },
+      "piece": {
+        "id": piece.id
+      }
     }
-    return this.http.post(this.beginPath+"play",JSON.stringify(data))
-        .toPromise()
-        .then((response: Response) => response.json());
+    return this.http.post(this.beginPath + "play", JSON.stringify(data))
+      .toPromise()
+      .then((response: Response) => response.json());
   }
 }
 
 export default Exchange;
- 
+
