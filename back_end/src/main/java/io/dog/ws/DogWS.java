72a131a33bfff1d4f1092807c2f9b094d5a16081
jbog77 <bogota.jeremy@gmail.com>
2017-04-19 08:44:40 +0200

    Deleted useless imports

--- back_end/src/main/java/io/dog/ws/DogWS.java
+++ back_end/src/main/java/io/dog/ws/DogWS.java
@@ -8,7 +8,6 @@ import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -20,9 +19,9 @@ import io.dog.dto.Deck;
 import io.dog.dto.GameBoard;
 import io.dog.dto.Piece;
 import io.dog.dto.Player;
+import io.dog.service.CreateService;
 import io.dog.service.LoadService;
 import io.dog.service.UpdateService;
-import io.dog.service.CreateService;
 
 @Path("dog")
 @Produces(MediaType.APPLICATION_JSON)
